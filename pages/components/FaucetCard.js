c4f1305471b5f2c5f359c28981de46c9a42bc21f
Monarch <92059211+Monarch47@users.noreply.github.com>
2022-09-17 01:01:50 +0530

    before adding detectEthprovider

---
+++ pages/components/FaucetCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const FaucetCard = (props) => {
+    return (
+        <div>
+            <div className="p-6 flex flex-col gap-4 items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Current Balance</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">10eth</p>
+                <div className='w-full flex justify-between gap-10'>
+                    <button href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Donate
+                        <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </button>
+                    <button href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        withdraw
+                        <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </button>
+                </div>
+                {/* <button href="#" className="w-3/4 py-2 px-3 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                    onClick={props.requestAccounts}>
+                    Enable Metamask
+                </button> */}
+            </div>
+
+        </div>
+    )
+}
+
+export default FaucetCard
